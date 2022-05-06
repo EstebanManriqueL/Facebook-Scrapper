697c585eaadb67fc7d354d1dfd3254b215c77923
EstebanManriqueL <82970873+EstebanManriqueL@users.noreply.github.com>
2022-05-06 12:44:27 -0500

    Documentación funciones y archivo requerimientos

--- scrapper_v3.py
+++ scrapper_v3.py
@@ -9,22 +9,19 @@ import sys
 import random
 from constants import *
 
-# pip install selenium
-# pip install beautifulsoup4
-
 recovered_posts = 0
 posts_selenium_ids = set()
 
-#Set Facebook Credentials
 def read_credentials():
+    """Set Facebook Credentials"""
     with open("./credentials.txt") as file:
         user = file.readline().split('"')
         user = user[0][0:len(user[0])-1]
         password = file.readline().split('"')[0]
     return(user, password)
 
-#Login with Credentials
 def login():
+    """Login with Credentials"""
     user, password = read_credentials()
     BROWSER.get("http://facebook.com")
     BROWSER.maximize_window()
@@ -34,9 +31,10 @@ def login():
     time.sleep(1)
     BROWSER.find_element(By.NAME, "login").click()
     time.sleep(5)
+    print("Inicio de sesión exitoso")
 
-#Auxiliary function to search "See more buttons"
 def seeMoreButtons(comm):
+    """Auxiliary function to search See more buttons"""
     buttons = comm.find_elements(By.CSS_SELECTOR, "div.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.gpro0wi8.oo9gr5id.lrazzd5p")
     if buttons:
         for button in buttons:
@@ -49,8 +47,8 @@ def seeMoreButtons(comm):
                     click.perform()
             except: pass
 
-#Auxiliary function to search and click for all buttons used to display more comments
 def moreCommentsButtons(comments_area):
+    """Auxiliary function to search and click for all buttons used to display more comments"""
     while True:
         #Evitar clicks en videos
         more_comments_buttons = comments_area.find_elements(By.CSS_SELECTOR, "div.oajrlxb2.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.f1sip0of.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.lzcic4wl.bp9cbjyn.m9osqain.buofh1pr.g5gj957u.p8fzw8mz.gpro0wi8")
@@ -62,8 +60,8 @@ def moreCommentsButtons(comments_area):
                 ActionChains(BROWSER).move_to_element(button).click().perform()
                 time.sleep(0.5)
 
-#Auxiliary function to configure FB's group to order posts based on publication date (descending order)
 def newestPosts():
+    """Auxiliary function to configure FB's group to order posts based on publication date (descending order)"""
     newest_posts_possible_buttons = BROWSER.find_elements(By.CSS_SELECTOR, "div.oajrlxb2.gs1a9yip.g5ia77u1.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.mg4g778l.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.l9j0dhe7.i1ao9s8h.esuyzwwr.f1sip0of.du4w35lb.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.pq6dq46d.btwxx1t3.abiwlrkh.p8dawk7l.lzcic4wl")
     if len(newest_posts_possible_buttons) > 0:
         for button in newest_posts_possible_buttons:
@@ -78,13 +76,13 @@ def newestPosts():
                         time.sleep(2)
                         return
 
-#Creates JSON file to dump scrapped data
 def createJSON(info):
+    """Creates JSON file to dump scrapped data"""
     with open("sampleNewFlow.json", "w", encoding="utf-8") as outfile:
         outfile.write(info)
 
-#Write new post information in JSON file
 def writeInJSON(post_info):
+    """Write new post information in JSON file"""
     with open("sampleNewFlow.json", "r+", encoding="utf-8") as outfile:
         existing_data = json.load(outfile)
         existing_data.append(post_info)
@@ -92,32 +90,29 @@ def writeInJSON(post_info):
         json.dump(existing_data, outfile, indent=4, ensure_ascii=False)
         time.sleep(0.3)
 
-#Scroll down in the group's page
 def scrollDown(groupPageLength):
+    """Scroll down in the group's page"""
     scrollCount = -1
     match = False
-
     while not match:
         getPosts(scrollCount)
         time.sleep(random.randint(2,4))
-        
         scrollCount += 1
         BROWSER.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        
         if scrollCount == groupPageLength:
             match = True
 
-#Obtain html/raw information
 def getToGroup():
+    """Obtain html/raw information"""
     BROWSER.get(TEST_GROUP)
     time.sleep(5)
     newestPosts()
     groupPageLength = BROWSER.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     
-    scrollDown(3)
+    scrollDown(1)
 
-#Identify posts HTML elements
 def getPosts(scrollCount):
+    """Identify posts HTML elements"""
     posts = BROWSER.find_elements(By.CLASS_NAME, "du4w35lb.k4urcfbm.l9j0dhe7.sjgh65i0")
     global recovered_posts
     for post in posts:
@@ -143,8 +138,8 @@ def getPosts(scrollCount):
             recovered_posts += 1
             print('Se han recuperado {0} posts'.format(recovered_posts))
 
-#Obtain message contained in the group's post
 def getPostMessage(comm):
+    """Obtain message contained in the group's post"""
     texts = comm.find_elements(By.CLASS_NAME, "d2edcug0.hpfvmrgz.qv66sw1b.c1et5uql.lr9zc1uh.a8c37x1j.fe6kdd0r.mau55g9w.c8b282yb.keod5gw0.nxhoafnm.aigsh9s9.d3f4x2em.iv3no6db.jq4qci2q.a3bd9o3v.b1v8xokw.oo9gr5id.hzawbc8m")
     texts = texts + comm.find_elements(By.XPATH, ".//div[@class='hv4rvrfc dati1w0a jb3vyjys qt6c0cv9']")
     texts = texts + comm.find_elements(By.XPATH, ".//div[@class='ll8tlv6m j83agx80 jifvfom9 l9j0dhe7 k4urcfbm']")
@@ -166,8 +161,8 @@ def getPostMessage(comm):
     final_text.strip()
     return final_text[:-1]
 
-#Obtain post link and id
 def extractPostLinkId(comm):
+    """Obtain post link and id"""
     link_elements = comm.find_elements(By.CLASS_NAME, "oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.gmql0nx0.gpro0wi8.b1v8xokw")
     if link_elements:
         index = 0
@@ -176,7 +171,7 @@ def extractPostLinkId(comm):
             date_element = link_elements[index]
             if not any(word in date_element.text for word in PAID_PARTNERSHIPS_WORDS):
                 ActionChains(BROWSER).move_to_element(date_element).perform()
-                time.sleep(random.randint(4,5))
+                time.sleep(random.randint(3,4))
                 try:      
                     date_html_component = BROWSER.find_element(By.CSS_SELECTOR, "div.j34wkznp.qp9yad78.pmk7jnqg.kr520xx4.hzruof5a")
                 except:
@@ -184,12 +179,12 @@ def extractPostLinkId(comm):
                 try:
                     BROWSER.execute_script("window.scrollBy(0, -40);")
                     ActionChains(BROWSER).move_to_element(date_element).perform()
-                    time.sleep(3)
+                    time.sleep(2)
                     date_html_component = BROWSER.find_element(By.CSS_SELECTOR, "div.j34wkznp.qp9yad78.pmk7jnqg.kr520xx4.hzruof5a")
                 except:
                     BROWSER.execute_script("window.scrollBy(0, -100);")
                     ActionChains(BROWSER).move_to_element(date_element).perform()
-                    time.sleep(3)
+                    time.sleep(2)
                     date_html_component = BROWSER.find_element(By.CSS_SELECTOR, "div.j34wkznp.qp9yad78.pmk7jnqg.kr520xx4.hzruof5a")
                     
                 date, hour = extractPostDateHour(str(date_html_component.text))
@@ -208,8 +203,8 @@ def extractPostLinkId(comm):
             return post_link, post_id, date, hour 
     return "", "", date, hour  
 
-#Extract post creator user information
 def extractPostUser(comm):
+    """Extract post creator user information"""
     user_element = comm.find_element(By.CLASS_NAME, "d2edcug0.hpfvmrgz.qv66sw1b.c1et5uql.lr9zc1uh.a8c37x1j.fe6kdd0r.mau55g9w.c8b282yb.keod5gw0.nxhoafnm.aigsh9s9.d3f4x2em.iv3no6db.jq4qci2q.a3bd9o3v.b1v8xokw.m9osqain.hzawbc8m")
     if user_element:
         user_element = user_element.find_element(By.CLASS_NAME, "oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl.gpro0wi8.oo9gr5id.lrazzd5p")
@@ -232,8 +227,8 @@ def extractPostUser(comm):
                     user_id = link[diagonal_indexes[2]+1:diagonal_indexes[3]-1]
                 return user_name, user_link, user_id  
 
-#Extract post's date and hour of publication
 def extractPostDateHour(comm):
+    """Extract post's date and hour of publication"""
     try:
         day = re.search(DATE_DAY_REGEX, comm).group()
         if len(day) == 1:
@@ -249,8 +244,8 @@ def extractPostDateHour(comm):
                     return day + "/" + MONTHS_DICTONARY[month] + "/" + year, hour
     except Exception: return "01/01/1999", "00:00"
 
-#Extract post's total reactions and shares
 def extractReactionTotalAndShares(comm):
+    """Extract post's total reactions and shares"""
     total_reactions = 0
     total_shares = 0
     try:
@@ -275,8 +270,8 @@ def extractReactionTotalAndShares(comm):
         total_shares = int(total_shares[0])
     return total_reactions, total_shares
 
-#Extract total amount of comments, as well as its information
 def extractTotalComments(comm):
+    """Extract total amount of comments, as well as its information"""
     total_comments = 0
     comments = []
     try:
@@ -300,8 +295,8 @@ def extractTotalComments(comm):
         comments = extractPostComments(comm)
     return total_comments, comments
 
-#Auxilairy function to make sure All Comments mode is activated
 def defineAllComments(comm):
+    """Auxilairy function to make sure All Comments mode is activated"""
     try:
         comment_list_buttons = comm.find_elements(By.CSS_SELECTOR, "div.oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.n00je7tq.arfg74bv.qs9ysxi8.k77z8yql.l9j0dhe7.abiwlrkh.p8dawk7l.lzcic4wl")
         comment_list_button = [x for x in comment_list_buttons if x.get_attribute("aria-label") not in PLAY_VIDEO_TEXTS]
@@ -317,8 +312,8 @@ def defineAllComments(comm):
         ActionChains(BROWSER).move_to_element(comment_list_button[0]).click().perform()
     except: pass
 
-#Function to extract information from comments regarding any post
 def extractPostComments(comm):
+    """Function to extract information from comments regarding any post"""
     comments_area = comm.find_element(By.CSS_SELECTOR, "div.cwj9ozl2.tvmbv18p")
     post_comments = list()
     seeMoreButtons(comments_area)
@@ -342,8 +337,8 @@ def extractPostComments(comm):
             post_comments.append({"author":comment_author, "text":comment_text, "reactions":comment_reactions})
     return post_comments
 
-#Unique function to be executed in order for the scrapper to work
 def unifyingFunctions():
+    """Unique function to be executed in order for the scrapper to work"""
     login()
     startTime = time.time()
     getToGroup()
